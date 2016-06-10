8fe4afb0832e6abde928e955eacd51ef638d2ad3
Ngo Minh Hung <hungnm@nsvc.com>
2016-06-10 12:43:15 +0700

    fixx

--- StoreUI/public_html/viewmodel/indexViewModel.js
+++ StoreUI/public_html/viewmodel/indexViewModel.js
@@ -12,12 +12,40 @@ function Product(id, name, genre, description, price, imageFileName){
     self.price = price;
     self.imageFileName = imageFileName;
 }
+
+function CartItem(product, quantity){
+    var self = this;
+    self.p = ko.observable(product);
+    self.quantity = ko.observable(quantity);
+    self.total = ko.computed(function(){
+        var price = self.product.price;
+        return self.quantity() * price;
+    });
+}
+
+
 function IndexViewModel(){
     var self = this;
-    self.listProducts = ko.observableArray([
+    self.featuredProducts1 = ko.observableArray([
         new Product(1,'test', 'test', 'test', 10, 'http://placehold.it/350x260'),
-        new Product(2,'test', 'test', 'test', 10, 'http://placehold.it/350x260'),
-        new Product(3,'test', 'test', 'test', 10, 'http://placehold.it/350x260')
+        new Product(2,'test', 'test', 'test', 11, 'http://placehold.it/350x260'),
+        new Product(3,'test', 'test', 'test', 12, 'http://placehold.it/350x260')
     ]);
+    self.featuredProducts2 = ko.observableArray([
+        new Product(1,'test', 'test', 'test', 13, 'http://placehold.it/350x260'),
+        new Product(2,'test', 'test', 'test', 14, 'http://placehold.it/350x260'),
+        new Product(3,'test', 'test', 'test', 15, 'http://placehold.it/350x260')
+    ]);
+    self.lstCartItem = ko.observableArray([
+        new CartItem(self.featuredProducts1[0], 1),
+        new CartItem(self.featuredProducts2[2], 2), 
+        new CartItem(self.featuredProducts1[2], 3)
+    ]);
+    self.subtotal = ko.computed(function(){
+       var total = 0;
+       for (var i = 0; i < self.lstCartItem.length; i++)
+           total += self.lstCartItem[i].cartItem().total;
+       return total;
+    });
 }
 ko.applyBindings(new IndexViewModel());
